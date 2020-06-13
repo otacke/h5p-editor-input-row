7e7dfbf5f27b49f83ce9ad0bda3a4afcff9e99b8
Oliver Tacke <o.tacke@posteo.de>
2020-06-13 15:02:43 +0200

    Fix sanitization of params

--- src/scripts/h5peditor-input-row.js
+++ src/scripts/h5peditor-input-row.js
@@ -13,7 +13,7 @@ class InputRow extends H5P.EventDispatcher {
 
     this.parent = parent;
     this.field = field;
-    this.params = params = {};
+    this.params = params || {};
     this.setValue = setValue;
 
     this.field.inputrow = this.field.inputrow || {};
