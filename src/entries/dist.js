11d630298c1a07e9aa1a60db1a1c2e716edabe64
Oliver Tacke <o.tacke@posteo.de>
2023-07-11 13:09:05 +0200

    Modernize build chain

---
+++ src/entries/dist.js
@@ -0,0 +1,5 @@
+import "@styles/h5peditor-input-row.scss";
+import InputRow from "@scripts/h5peditor-input-row";
+
+// Load library
+H5PEditor.widgets.inputrow = InputRow;
--- src/entries/h5peditor-input-row.js
+++
@@ -1,6 +0,0 @@
-import "../styles/h5peditor-input-row.css";
-import InputRow from "../scripts/h5peditor-input-row";
-
-// Load library
-H5PEditor = H5PEditor || {};
-H5PEditor.widgets.inputrow = InputRow;
